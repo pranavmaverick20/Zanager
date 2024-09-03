c5f4a2fe2948ad8c1edbbfacfd86dc9a69a779da
pranavmaverick20 <pranavahuja20@gmail.com>
2024-09-03 18:05:36 +0530

    Added models for user and task

--- tasker-backend/models/task.js
+++ tasker-backend/models/task.js
@@ -1,9 +1,38 @@
 const mongoose = require('mongoose');
-const taskSchema = mongoose.Schema(
+const task_schema = mongoose.Schema(
     {
         title: {
-            requied: true,
+            required: true,
             type: String
+        },
+        description: {
+            required: true,
+            type: String
+        },
+        tags: {
+            required: false,
+            type: String
+        },
+        status: {
+            required: true,
+            type: Number
+        },
+        assigneeId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        reportedId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        deadline: {
+            required: false,
+            type: Date
         }
+    },
+    {
+        timestamps: true
     }
-)
+);
+const task_model = mongoose.model("Task", task_schema);
+module.exports = task_model;
---
+++ tasker-backend/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const user_schema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isVerified: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const user_model = mongoose.model("User", user_schema);
+module.exports = user_model;
